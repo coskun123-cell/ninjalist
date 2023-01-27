a6680857fba52c95af75801ba3c9ef5f7dc656d9
Coskun Cabuk <coskuncabuk@Coskuns-MacBook-Air.local>
2023-01-27 14:40:32 +0000

    First Commit

---
+++ pages/about.js
@@ -0,0 +1,28 @@
+import Head from "next/head";
+const About = () => {
+  return (
+    <>
+      <Head>
+        <title>Ninja List | About</title>
+        <meta name="keywords" content="about" />
+      </Head>
+      <div>
+        <h1>About</h1>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam quia
+          pariatur, accusantium vitae, eveniet culpa exercitationem magni eaque,
+          ut quo architecto. Soluta eum deserunt modi. Eligendi est blanditiis
+          beatae quisquam.
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam quia
+          pariatur, accusantium vitae, eveniet culpa exercitationem magni eaque,
+          ut quo architecto. Soluta eum deserunt modi. Eligendi est blanditiis
+          beatae quisquam.
+        </p>
+      </div>
+    </>
+  );
+};
+
+export default About;
---
+++ pages/index.js
@@ -0,0 +1,34 @@
+import Head from "next/head";
+import styles from "../styles/Home.module.css";
+import Navbar from "../comps/Navbar";
+import Footer from "../comps/Footer";
+import Link from "next/link";
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Ninja List | Home</title>
+        <meta name="keywords" content="ninjas" />
+      </Head>
+      <div>
+        <h1 className={styles.title}>Home Page</h1>
+        <p className={styles.text}>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam quia
+          pariatur, accusantium vitae, eveniet culpa exercitationem magni eaque,
+          ut quo architecto. Soluta eum deserunt modi. Eligendi est blanditiis
+          beatae quisquam.
+        </p>
+        <p className={styles.text}>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam quia
+          pariatur, accusantium vitae, eveniet culpa exercitationem magni eaque,
+          ut quo architecto. Soluta eum deserunt modi. Eligendi est blanditiis
+          beatae quisquam.
+        </p>
+        <Link href="/ninjas" legacyBehavior>
+          <a className={styles.btn}>See Ninja Listing</a>
+        </Link>
+      </div>
+    </>
+  );
+}
